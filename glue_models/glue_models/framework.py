2ae0abba7ab3a903bbbfa2eaeb0540047ef1ab8d
md <deeplearnmd@gmail.com>
2020-01-12 14:47:48 +0000

    add Matthews Correlation

--- glue_models/glue_models/framework.py
+++ glue_models/glue_models/framework.py
@@ -131,12 +131,12 @@ def matthews_corr(preds, y):
     y_pr_torch = torch.round(torch.sigmoid(preds))
     y_tr_torch = torch.round(y)
 
-    tp = ((y_tr_torch==1) & (y_pr_torch==1)).sum()
-    tn = ((y_tr_torch==0) & (y_pr_torch==0)).sum()
-    fp = ((y_tr_torch==0) & (y_pr_torch==1)).sum()
-    fn = ((y_tr_torch==1) & (y_pr_torch==0)).sum()
+    tp = ((y_tr_torch==1) & (y_pr_torch==1)).sum().to('cpu').numpy()
+    tn = ((y_tr_torch==0) & (y_pr_torch==0)).sum().to('cpu').numpy()
+    fp = ((y_tr_torch==0) & (y_pr_torch==1)).sum().to('cpu').numpy()
+    fn = ((y_tr_torch==1) & (y_pr_torch==0)).sum().to('cpu').numpy()
 
-    mcc = (tp * tn - fp * fn) / torch.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+    mcc = (tp * tn - fp * fn) / np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
     #rounded_preds = rounded_preds.detach().cpu().numpy().flatten()
     #y = y.to('cpu').numpy().flatten()
     #correct = (rounded_preds == y).float() #convert into float for division 
